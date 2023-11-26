2d4f20623d6fcb33cc05b4bf63a7639403a59f85
FxRayHughes <bushuaichen@yeah.net>
2023-11-26 12:20:11 +0800

    取消了SQL的专有文件 合并到主config内

--- project/util-sql/src/main/kotlin/ink/work/taboopublicwork/utils/sql/SQLUtil.kt
+++ project/util-sql/src/main/kotlin/ink/work/taboopublicwork/utils/sql/SQLUtil.kt
@@ -1,11 +1,6 @@
 package ink.work.taboopublicwork.utils.sql
 
-import taboolib.module.configuration.Config
-import taboolib.module.configuration.ConfigFile
-
 object SQLUtil {
 
-    @Config(value = "sql.yml")
-    lateinit var config: ConfigFile
 
 }
